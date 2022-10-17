6f1fe42bad00581dd9dab8e05706b4c1cea3c13e
Kimdaeun0929 <102155713+Kimdaeun0929@users.noreply.github.com>
2022-10-17 23:43:20 +0900

    Add files via upload

---
+++ 221011.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define SEED 17
+int MULT = 25173;
+int INC = 13849;
+int MOD = 65536;
+
+static unsigned int seed = SEED;
+
+unsigned random_i(void)
+{
+	seed = (MULT * seed + INC) % MOD;
+		return seed;
+}
+
+double random_f(void)
+{
+	seed = (MULT * seed + INC) % MOD;
+	return seed / (double)MOD;
+}
